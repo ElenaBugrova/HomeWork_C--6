12772c9a93034d758e1e49d2ee93c3c0b4aae245
ElenaBugrova <evilisia@mail.ru>
2022-12-13 15:07:45 +0300

    homework

---
+++ --41/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько
+//чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2  
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Введите числа через пробел: ");
+string elements = Console.ReadLine(); //считывает элементы и заносит их в строки
+int[] baseArray = GetArrayFromString(elements); //создает массив baseArray используя метод GetArrayFromString
+
+int[] GetArrayFromString(string stringArray) //метод перевода элементов строки в элементы int
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int count=0;
+foreach (int item in baseArray)
+    {
+        if (item>0) count++; // если элемент больше 0, то count+1 прибавляет
+    }
+
+Console.Write($"--> {count}");
---
+++ --43/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух
+//прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1,
+// b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.WriteLine("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+void FindXY (double a1, double c1, double a2, double c2)
+{
+    double x = (a2-a1)/(c1-c2);
+    double y = c1*x+a1; 
+    Console.WriteLine($"Координата х= {x}");
+    Console.WriteLine($"Координата у= {y}");
+}
+
+FindXY(b1,k1,b2,k2);
+
+
